8ddd5d5f3af540159c7ccdecc125bd8e744c5fd5
wushuaiun <wushuaiun@macbookpro.lan>
2025-10-09 11:58:56 +0800

    IU-2025.2.3 <wushuaiun@MacBookPro.lan Create csvSettings.xml, textmate.xml, project.default.xml, diff.xml, Enum.java, terminal-font.xml, Interface.java, window.layouts.xml, Default.xml, file.template.settings.xml, find.xml, javarebel.xml, log-categories.xml, nodejs.xml, k2-feedback.xml, Tag Library Descriptor.tld, AIOnboardingPromoWindowAdvisor.xml, editor.xml, profilerRunConfigurations.xml, debugger.xml, Jspx File.jspx, updates.xml, ide.general.xml, trusted-paths.xml, ide-features-trainer.xml, advancedSettings.xml, web-types-npm-loader.xml, profiler.xml, terminal.xml, xpath.xml, ignored-suggested-plugins.xml, ui.lnf.xml, JAX-RS Resource.java, DontShowAgainFeedbackService.xml, filetypes.xml, package-info.java, window.state.xml, codeInsightSettings.xml, editor-font.xml, colors.scheme.xml, dataViewsSettings.xml, databaseDrivers.xml, customization.xml, Class.java, jdk.table.xml, JAX-RS Client.java, Jsp File.jsp, settingsSync.xml, path.macros.xml, proxy.settings.xml, databaseSettings.xml, grazie_global.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @author ${USER}
+* @since 1.0
+*/
+public class ${NAME} {
+}
---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};
+
+#end
+#parse("File Header.java")
+/**
+* @author ${USER}
+* @since 1.0
+*/
+public enum ${NAME} {
+}
